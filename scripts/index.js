d249337749dc28084483169780f757af52f55a03
sebastian Osorio <144757345+samuraypro7767@users.noreply.github.com>
2025-02-28 06:43:00 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -21,7 +21,7 @@ function randomLetter() {
 
 // Temporizador
 function contadorAtras() {
-    let tiempoRestante = 2;
+    let tiempoRestante = 59;
     let intervalo = setInterval(() => {
         if (tiempoRestante > 0) {
             document.getElementById('time').innerText = `00:${tiempoRestante < 10 ?  + tiempoRestante : tiempoRestante}`;
